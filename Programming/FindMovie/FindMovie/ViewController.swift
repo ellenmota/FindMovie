3ac123954d28acbb9dbdfa04bfaa835a28c4d5a3
Ellen Mota <ellenmota29@gmail.com>
2017-06-18 18:06:06 -0300

    Starting the project in Xcode

---
+++ Programming/FindMovie/FindMovie/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FindMovie
+//
+//  Created by Ellen Bianca Mota de Carvalho on 18/06/17.
+//  Copyright © 2017 Ellen Bianca Mota de Carvalho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
